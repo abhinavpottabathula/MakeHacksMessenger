746425168a0734dd98653ce9174daa149ecbc815
RTavare <rohit.t797@yahoo.com>
2015-11-15 07:15:41 -0800

    Extended functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var screen = "login";
-var aspect = 1.5
+var aspect = 1.5;
+var aspect2 = 1.7;
 var username;
+var password;
+var myFirebaseRef = new Firebase("https://dicoapp.firebaseio.com/");
 
 $(document).ready(function() {
 
@@ -12,21 +15,74 @@ $(document).ready(function() {
 		$("#login").stop().animate({color: "white",backgroundColor: "#ff314f"}, 250);
 	});
 	$("#login").mouseleave(function() {
-		$("#login").stop().animate({color: "#ff314f", backgroundColor: 'rgba(0, 0, 0, 0)'}, 250);
+		$("#login").stop().animate({color: "white", backgroundColor: 'rgba(0, 0, 0, 0)'}, 250);
+	});
+  $("#sign").mouseenter(function() {
+		$("#sign").stop().animate({color: "white",backgroundColor: "#ff314f"}, 250);
+	});
+	$("#sign").mouseleave(function() {
+		$("#sign").stop().animate({color: "white", backgroundColor: 'rgba(0, 0, 0, 0)'}, 250);
+	});
+  $("#create").mouseenter(function() {
+		$("#create").stop().animate({color: "white",backgroundColor: "#ff314f"}, 250);
+	});
+	$("#create").mouseleave(function() {
+		$("#create").stop().animate({color: "white", backgroundColor: 'rgba(0, 0, 0, 0)'}, 250);
+	});
+  $("#log").mouseenter(function() {
+		$("#log").stop().animate({color: "white",backgroundColor: "#ff314f"}, 250);
+	});
+	$("#log").mouseleave(function() {
+		$("#log").stop().animate({color: "white", backgroundColor: 'rgba(0, 0, 0, 0)'}, 250);
 	});
 
   $("#login").click(function() {
     if($("#username").val() != "" && $("#password").val() != "") {
       username = $("#username").val();
+      password = $("#password").val();
       $("#username").val("");
+      $("#password").val("");
+      //if()
       screen = "menu";
+      $("#welcome").text("Welcome " + username);
       updateScreen();
     }
+  });
+  $("#sign").click(function() {
+      screen = "sign";
+      updateScreen();
+  });
+  $("#log").click(function() {
+      screen = "login";
+      updateScreen();
+  });
+  $("#create").click(function() {
+    if($("#user").val() != "" && $("#pass").val() != "") {
+      username = $("#user").val();
+      $("#user").val("");
+      $("#pass").val("");
+      screen = "login";
+      updateScreen();
+    }
+  });
+
+  $("#dico_ch_a").click(function() {
+    screen = "chat";
+    updateScreen();
+  });
+  $("#dico_ch_b").click(function() {
+    screen = "chat";
+    updateScreen();
+  });
+  $("#dico_ch_c").click(function() {
+    screen = "chat";
+    updateScreen();
   });
 
   $(window).resize(function() {
     scale();
   });
+
 
 });
 
@@ -35,14 +91,22 @@ function updateScreen() {
     $(".login").show();
     $(".chat").hide();
     $(".menu").hide();
+    $(".sign").hide();
   } else if(screen == "chat") {
     $(".menu").hide();
     $(".login").hide();
     $(".chat").show();
+    $(".sign").hide();
   } else if(screen == "menu") {
     $(".menu").show();
     $(".login").hide();
     $(".chat").hide();
+    $(".sign").hide();
+  } else if(screen == "sign") {
+    $(".menu").hide();
+    $(".login").hide();
+    $(".chat").hide();
+    $(".sign").show();
   }
 }
 
@@ -51,5 +115,15 @@ function scale() {
     $(".login").css("background-size", "100vw auto");
   } else {
     $(".login").css("background-size", "auto 100vh");
+  }
+  if($(window).width()/$(window).height() >= aspect) {
+    $(".sign").css("background-size", "100vw auto");
+  } else {
+    $(".sign").css("background-size", "auto 100vh");
+  }
+  if($(window).width()/$(window).height() >= aspect2) {
+    $("#menuheader").css("background-size", "100vw auto");
+  } else {
+    $("#menuheader").css("background-size", "auto 100vh");
   }
 }
